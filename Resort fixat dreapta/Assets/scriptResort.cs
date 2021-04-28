91a4d3e2863d5f5fa2153ac2ba25540e6b912dd2
radumsk <raducostache@mail.ru>
2021-04-29 02:09:15 +0300

    Am adaugat resortul fixat la dreapta. Nu stiu daca e chiar 100% bun, dar pare ca merge. Pentru cel fixat la stanga trebuie doar pus - in loc de + si invers in unele locuri. Este important si scriptul pe care l-am pus la bila!!!

---
+++ Resort fixat dreapta/Assets/scriptResort.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class scriptResort : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static int stadiu = 0;
+    public float dl, change = 0, k = 500, E;
+    const float scaleDif = 0.2f;
+    GameObject Sphere;
+    Rigidbody sphereRigidBody;
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //Debug.Log(change);
+        if (stadiu == 2)
+        {
+            if (change <= 0)
+            {
+                sphereRigidBody.isKinematic = false;
+                sphereRigidBody.AddForce(new Vector3(-k * dl, 0, 0));
+                stadiu = 0;
+            }
+            else
+            {
+                sphereRigidBody.transform.position -= new Vector3(scaleDif, 0, 0);
+                change -= scaleDif;
+                transform.localScale -= new Vector3(scaleDif, 0, 0);
+            }
+            Sphere.GetComponent<controller>().stadiu = stadiu;
+        }
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name != "Cube")
+        {
+            Sphere = GameObject.Find(col.gameObject.name);
+            sphereRigidBody = Sphere.GetComponent<Rigidbody>();
+
+            //Debug.Log(Sphere.GetComponent<controller>().prevVelocity);
+            
+            if (stadiu == 0)
+            {
+                stadiu = 1;
+                change = 0;
+                dl = Mathf.Sqrt(sphereRigidBody.mass / k) * Sphere.GetComponent<controller>().prevVelocity;
+            }
+            if (stadiu == 1)
+            {
+                if (change >= dl)
+                {
+                    stadiu = 2;
+                    sphereRigidBody.isKinematic = true;
+                }
+                else
+                {
+                    change += scaleDif;
+                    transform.localScale += new Vector3(scaleDif, 0, 0);
+                }
+            }
+            Sphere.GetComponent<controller>().stadiu = stadiu;
+            Debug.Log(stadiu);
+        }
+    }
+}
